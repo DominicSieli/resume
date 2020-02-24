89356583c42380fc108f31f407b2095b48c135dc
Ashley Olson <dominicsieli@gmail.com>
2020-02-23 19:06:49 -0700

    Updated projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -7,7 +7,7 @@ const SecondPage = () => (
     <h1>Current Projects</h1>
     <h3><a href="https://github.com/DominicSieli/Samurai-Engine">Samurai Game Engine</a></h3>
     <p>
-      This is an open source game engine.
+      This is an open source game engine built using a data oriented design architecture.
     </p>
   </Layout>
 )
