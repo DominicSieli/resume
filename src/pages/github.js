bc7203de4e27f52e5843ce310ade30288a0637a8
Ashley Olson <dominicsieli@gmail.com>
2020-02-25 00:57:16 -0700

    Added github page

---
+++ src/pages/github.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const SecondPage = () => (
+  <Layout>
+    <h1>GitHub</h1>
+    <h3><a href="https://github.com/DominicSieli">GitHub</a></h3>
+  </Layout>
+)
+
+export default SecondPage
