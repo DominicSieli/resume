c1f5cbd14dcb9ec27452b46b8e51ce6aca3e791b
Ashley Olson <dominicsieli@gmail.com>
2020-02-25 00:14:11 -0700

    Updated github url

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
       },
       {
         name: 'GitHub',
-        link:'https://github.com/DominicSieli'
+        link:'https://github.com/DominicSieli/Samurai-Engine'
       }
     ],
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
