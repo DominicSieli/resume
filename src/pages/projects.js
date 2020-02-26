7e88e388c9644d53026ee56af11ce2b87230f00b
Ashley Olson <dominicsieli@gmail.com>
2020-02-26 08:01:15 -0700

    Updated projects and accreditations pages

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,9 +5,9 @@ import Layout from "../components/layout"
 const SecondPage = () => (
   <Layout>
     <h1>Projects</h1>
-    <h3><a href="https://github.com/DominicSieli/Samurai-Engine">Samurai Game Engine</a></h3>
+    <h3><a href="https://github.com/DominicSieli/Samurai-Engine">Samurai</a></h3>
     <p>
-      The Samurai Engine is an experimental game engine designed to be minimal, extensible, and powerful.
+      Samurai is an experimental game engine designed to be minimal, extensible, and powerful.
     </p>
   </Layout>
 )
