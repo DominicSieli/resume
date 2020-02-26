9b663953ff2fe12680ddc654cb7a426ddd864e80
Ashley Olson <dominicsieli@gmail.com>
2020-02-26 07:08:57 -0700

    Updated links page

--- src/pages/links.js
+++ src/pages/links.js
@@ -4,10 +4,10 @@ import Layout from "../components/layout"
 
 const SecondPage = () => (
   <Layout>
-    <h1>GitHub</h1>
-    <h3><a href="https://github.com/DominicSieli">Git Hub</a></h3>
-    <h3><a href="https://leetcode.com/dominus64">Leet Code</a></h3>
-    <h3><a href="https://www.youtube.com/channel/UCBogkPwNLPfMaO1_cAzmq1w">You Tube</a></h3>
+    <h1>Links</h1>
+    <h3><a href="https://github.com/DominicSieli">GitHub</a></h3>
+    <h3><a href="https://leetcode.com/dominus64/">LeetCode</a></h3>
+    <h3><a href="https://www.youtube.com/channel/UCBogkPwNLPfMaO1_cAzmq1w">YouTube</a></h3>
     <h3><a href="https://stackoverflow.com/users/12935754/dominic-sieli">Stack Overflow</a></h3>
   </Layout>
 )
