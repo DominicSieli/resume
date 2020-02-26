b0fb436751d2d8c71a9a22a76bea805eab06c5da
Ashley Olson <dominicsieli@gmail.com>
2020-02-26 08:14:55 -0700

    Updated accreditations page

--- src/pages/accreditation.js
+++ src/pages/accreditation.js
@@ -7,18 +7,21 @@ const AboutPage = () => (
     <h1>Accreditation</h1>
     <h2><a href="https://www.mobygames.com/developer/sheet/view/developerId,426098/">Alan Wake</a></h2>
     <p>
-      Dominic's accreditation: Motion capture QA technician.                                           
-      Alan Wake is an Xbox 360 title developed by Remedy Entertainment.
+      Platform: Xbox 360
+      <br>Developer: Remedy Entertainment
+      <br>Dominic's Accreditation: Motion capture QA technician
     </p>
     <h2><a href="https://www.microsoft.com/en-us/p/vector-blade/9pj4rv8ngjbn?activetab=pivot:overviewtab">Vector Blade</a></h2>
     <p>
-      Dominic's accreditation: Solo developer.                                                         
-      Vector Blade is an Xbox One title developed by Exploding Brain Games.
+      Platform: Xbox One
+      <br>Developer: Exploding Brain Games
+      <br>Dominic's Accreditation: Solo developer
     </p>
     <h2><a href="https://play.google.com/store/apps/details?id=com.Exploding_Brain_Games.Space_Shark_International&hl=en_US">Space Shark</a></h2>
     <p>
-      Dominic's accreditation: Solo developer.                                                         
-      Space Shark is an Google Play title developed by Exploding Brain Games.
+      Platform: Google Play
+      <br>Developer: Exploding Brain Games
+      <br>Dominic's Accreditation: Solo developer
     </p>
   </Layout>
 )
