1938ff4c98594ab90c382ee76a8bdd2f6934f033
Ashley Olson <dominicsieli@gmail.com>
2020-02-27 14:31:41 -0700

    Updated links page

--- src/pages/links.js
+++ src/pages/links.js
@@ -6,6 +6,7 @@ const SecondPage = () => (
   <Layout>
     <h1>Links</h1>
     <h3><a href="https://github.com/DominicSieli">GitHub</a></h3>
+    <h3><a href="https://www.youtube.com/channel/UCBogkPwNLPfMaO1_cAzmq1w?view_as=subscriber">YouTube</a></h3>
     <h3><a href="https://leetcode.com/dominus64/">LeetCode</a></h3>
     <h3><a href="https://stackoverflow.com/users/12935754/dominic-sieli">Stack Overflow</a></h3>
   </Layout>
