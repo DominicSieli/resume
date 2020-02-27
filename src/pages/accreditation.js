9c73e1cc0e6cd3a52df9cf798ea5ce1a9f7efdf8
Ashley Olson <dominicsieli@gmail.com>
2020-02-26 19:37:37 -0700

    Updated accreditation page

--- src/pages/accreditation.js
+++ src/pages/accreditation.js
@@ -8,24 +8,28 @@ const AboutPage = () => (
     <h2><a href="https://www.mobygames.com/developer/sheet/view/developerId,426098/">Alan Wake</a></h2>
     <p>
       Platform: Xbox 360
+      <br/>Release Date: 02/16/2012
       <br/>Developer: Remedy Entertainment
       <br/>Dominic's Accreditation: Motion capture QA technician
     </p>
     <h2><a href="https://www.microsoft.com/en-us/p/vector-blade/9pj4rv8ngjbn?activetab=pivot:overviewtab">Vector Blade</a></h2>
     <p>
       Platform: Xbox One
+      <br/>Release Date: 01/10/2019
       <br/>Developer: Exploding Brain Games
       <br/>Dominic's Accreditation: Solo developer
     </p>
     <h2><a href="https://play.google.com/store/apps/details?id=com.Exploding_Brain_Games.Space_Shark_International&hl=en_US">Space Shark</a></h2>
     <p>
       Platform: Google Play
+      <br/>Release Date: 06/27/2015
       <br/>Developer: Exploding Brain Games
       <br/>Dominic's Accreditation: Solo developer
     </p>
     <h2><a href="https://play.google.com/store/apps/details?id=com.Exploding_Brain_Games.Space_Shark_International&hl=en_US">Munchy Mo</a></h2>
     <p>
       Platform: Google Play
+      <br/>Release Date: 06/27/2013
       <br/>Developer: Exploding Brain Games
       <br/>Dominic's Accreditation: Solo developer
     </p>
