0a2c49ee19fc043cd12a7deefe0c4f7f8fcdf6d8
Ashley Olson <dominicsieli@gmail.com>
2020-02-27 14:16:43 -0700

    Updated projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -8,6 +8,7 @@ const SecondPage = () => (
     <h3><a href="https://github.com/DominicSieli/Samurai-Engine">Samurai</a></h3>
     <p>
       Dominic's Accreditation: Solo Developer
+      <br/>
       <br/>Samurai is an experimental game engine intended to be a learning exercise in game engine engineering.
     </p>
   </Layout>
