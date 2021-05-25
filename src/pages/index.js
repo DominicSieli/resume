0ea4d51743d52a325c0cbb035381a56c1947f5d3
dominicsieli <dominicsieli@gmail.com>
2021-05-24 17:25:39 -0700

    Updated Resume

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,30 +11,30 @@ const IndexPage = () => (
     <h1>Resume</h1>
     <p>
     SKILLS
-      • Programming: C, C++, and C#
-      • Game Engines: Unity, Unreal, and Godot
-      • Editors: Visual Studio, CodeBlocks, VS Code, and Vim
-      • Publishing Experience: Xbox One, and Google Play Store
+      Programming: C, C++, and C#
+      Game Engines: Unity, Unreal, and Godot
+      Editors: Visual Studio, CodeBlocks, VS Code, and Vim
+      Publishing Experience: Xbox One, and Google Play Store
     </p>
     <p>
     PUBLISHED GAMES
-      • Vector Blade (Xbox One) - Exploding Brain Games, 2019
-      • Space Shark (Google Play) - Exploding Brain Games, 2015
-      • Munchy Mo (Google Play) - Exploding Brain Games, 2014
-      • Alan Wake (Xbox 360) - Remedy Entertainment, 2010
+      Vector Blade (Xbox One) - Exploding Brain Games, 2019
+      Space Shark (Google Play) - Exploding Brain Games, 2015
+      Munchy Mo (Google Play) - Exploding Brain Games, 2014
+      Alan Wake (Xbox 360) - Remedy Entertainment, 2010
     </p>
     <p>
     WORK EXPERIENCE
       Exploding Brain Games LLC, Phoenix, AZ - Owner
       12/2013 - 04/2021
-      • Owner
-      • Solo Game Developer
+        Owner
+        Solo Game Developer
 
       CaptiveMotion LLC, Tempe, AZ - Performance Capture QA
       08/2009 - 06/2011
-      • Performance Capture Data Clean-Up
-      • Performance Capture Camera Calibration
-      • Performance Capture Marker and Actor Preparation
+        Performance Capture Data Clean-Up
+        Performance Capture Camera Calibration
+        Performance Capture Marker and Actor Preparation
     </p>
     <p>
     EDUCATION
