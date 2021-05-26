70a984e10f1c08cf80f2cade40f0704a3fc35d9c
dominicsieli <dominicsieli@gmail.com>
2021-05-25 21:09:11 -0700

    Updated colors

--- src/pages/contacts.js
+++ src/pages/contacts.js
@@ -5,11 +5,10 @@ import Layout from "../components/layout"
 const SecondPage = () => (
   <Layout>
     <h1>Contacts</h1>
-    
-    <p>    Phone: (602)448-9528
+    <p>
+           Phone: (602)448-9528
       <br/>Email: dominicsieli@gmail.com
     </p>
-
   </Layout>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,8 @@ const IndexPage = () => (
     <h1>Resume</h1>
 
     <h2>SKILLS</h2>
-    <p>    •  Programming: C/C++, and C#
+    <p>
+           •  Programming: C/C++, and C#
       <br/>•  Game Engines: Unity, Unreal, and Godot
       <br/>•  Editors: Visual Studio, CodeBlocks, VS Code, and Vim
       <br/>•  Publishing Experience: Xbox One, and Google Play Store
@@ -19,14 +20,15 @@ const IndexPage = () => (
 
     <h2>PUBLISHED GAMES</h2>
     <p>
-      <a href="https://www.microsoft.com/en-us/p/vector-blade/9pj4rv8ngjbn?activetab=pivot:overviewtab">Vector Blade</a> (Xbox One) - Exploding Brain Games, 2019
-      <a href="https://play.google.com/store/apps/details?id=com.Exploding_Brain_Games.Space_Shark_Free">Space Shark</a> (Google Play) - Exploding Brain Games, 2015
-      <a href="https://play.google.com/store/apps/details?id=com.Exploding_Brain.Munchy_Mo_Demo">Munchy Mo</a> (Google Play) - Exploding Brain Games, 2014
-      <a href="https://www.mobygames.com/developer/sheet/view/developerId,426098/">Alan Wake</a> (Xbox 360) - Remedy Entertainment, 2010
+           <a href="https://www.microsoft.com/en-us/p/vector-blade/9pj4rv8ngjbn?activetab=pivot:overviewtab">Vector Blade</a> (Xbox One) - Exploding Brain Games, 2019
+      <br/><a href="https://play.google.com/store/apps/details?id=com.Exploding_Brain_Games.Space_Shark_Free">Space Shark</a> (Google Play) - Exploding Brain Games, 2015
+      <br/><a href="https://play.google.com/store/apps/details?id=com.Exploding_Brain.Munchy_Mo_Demo">Munchy Mo</a> (Google Play) - Exploding Brain Games, 2014
+      <br/><a href="https://www.mobygames.com/developer/sheet/view/developerId,426098/">Alan Wake</a> (Xbox 360) - Remedy Entertainment, 2010
     </p>
 
     <h2>WORK EXPERIENCE</h2>
-    <p>      Exploding Brain Games LLC, Phoenix, AZ - Owner
+    <p>
+             Exploding Brain Games LLC, Phoenix, AZ - Owner
       <br/>  12/2013 - 04/2021
       <br/>  •  Owner
       <br/>  •  Solo Game Developer
@@ -39,12 +41,12 @@ const IndexPage = () => (
     </p>
 
     <h2>EDUCATION</h2>
-    <p>      ITT Technical Institute, Tempe AZ - Bachelor’s Degree
+    <p>
+             ITT Technical Institute, Tempe AZ - Bachelor’s Degree
       <br/>  03/2007 - 03/2011
       <br/>  Bachelor of Computer Science in Digital Entertainment and Game Design
       <br/>  GPA 3.42
     </p>
-    
   </Layout>
 )
 
