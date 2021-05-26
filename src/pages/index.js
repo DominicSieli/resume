6948242c89d17a855f3ef1ff7a9215825533bace
dominicsieli <dominicsieli@gmail.com>
2021-05-25 20:35:51 -0700

    Updated colors

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,11 +18,10 @@ const IndexPage = () => (
     </p>
 
     <h2>PUBLISHED GAMES</h2>
-    <p>    •  <h3><a href="https://www.microsoft.com/en-us/p/vector-blade/9pj4rv8ngjbn?activetab=pivot:overviewtab">Vector Blade</a></h3> (Xbox One) - Exploding Brain Games, 2019
-      <br/>•  <h3><a href="https://play.google.com/store/apps/details?id=com.Exploding_Brain_Games.Space_Shark_Free">Space Shark</a></h3> (Google Play) - Exploding Brain Games, 2015
-      <br/>•  <h3><a href="https://play.google.com/store/apps/details?id=com.Exploding_Brain.Munchy_Mo_Demo">Munchy Mo</a></h3> (Google Play) - Exploding Brain Games, 2014
-      <br/>•  <h3><a href="https://www.mobygames.com/developer/sheet/view/developerId,426098/">Alan Wake</a></h3> (Xbox 360) - Remedy Entertainment, 2010
-    </p>
+    <h3><a href="https://www.microsoft.com/en-us/p/vector-blade/9pj4rv8ngjbn?activetab=pivot:overviewtab">Vector Blade</a> (Xbox One) - Exploding Brain Games, 2019</h3>
+    <h3><a href="https://play.google.com/store/apps/details?id=com.Exploding_Brain_Games.Space_Shark_Free">Space Shark</a> (Google Play) - Exploding Brain Games, 2015</h3>
+    <h3><a href="https://play.google.com/store/apps/details?id=com.Exploding_Brain.Munchy_Mo_Demo">Munchy Mo</a> (Google Play) - Exploding Brain Games, 2014</h3>
+    <h3><a href="https://www.mobygames.com/developer/sheet/view/developerId,426098/">Alan Wake</a> (Xbox 360) - Remedy Entertainment, 2010</h3>
 
     <h2>WORK EXPERIENCE</h2>
     <p>      Exploding Brain Games LLC, Phoenix, AZ - Owner
