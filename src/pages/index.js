621a0c82970cfe18ec3ba0ecccc49b523101303a
dominicsieli <dominicsieli@gmail.com>
2021-10-14 14:46:09 -0700

    Updated Resume

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,15 +10,7 @@ const IndexPage = () => (
     <SEO title="Resume" />
     <h1>Resume</h1>
 
-    <h2>SKILLS</h2>
-    <p>
-           •  Programming: C/C++, and C#
-      <br/>•  Game Engines: Unity, Unreal, and Godot
-      <br/>•  Editors: Visual Studio, Code::Blocks, VS Code, and Vim
-      <br/>•  Publishing Experience: Xbox One, and Google Play Store
-    </p>
-
-    <h2>PUBLISHED GAMES</h2>
+    <h2>ACCREDITATION</h2>
     <p>
            •  <a href="https://www.microsoft.com/en-us/p/vector-blade/9pj4rv8ngjbn?activetab=pivot:overviewtab">Vector Blade</a> (Xbox One) - Exploding Brain Games, 2019
       <br/>•  <a href="https://play.google.com/store/apps/details?id=com.Exploding_Brain_Games.Space_Shark_Free">Space Shark</a> (Google Play) - Exploding Brain Games, 2015
@@ -26,30 +18,44 @@ const IndexPage = () => (
       <br/>•  <a href="https://www.mobygames.com/developer/sheet/view/developerId,426098/">Alan Wake</a> (Xbox 360) - Remedy Entertainment, 2010
     </p>
 
+    <h2>SKILLS</h2>
+    <p>
+           <b>Programming</b>
+      <br/>•  Experience with C, C++, and C#
+      <br/>•  Strong Knowledge of Data-Structure and Algorithm Design
+      <br/>
+      <br/><b>Game Development</b>
+      <br/>•  Experience with UI Design
+      <br/>•  Experience with Animation
+      <br/>•  Experience with 3D Modeling
+      <br/>•  Experience with Game Play Design
+      <br/>•  10 Years of Unity Game Engine Experience
+      <br/>
+      <br/><b>Publishing</b>
+      <br/>•  Experience Publishing to Xbox One
+      <br/>•  Experience Publishing to Google Play Store
+    </p>
+
     <h2>WORK EXPERIENCE</h2>
     <p>
-             Exploding Brain Games LLC, Phoenix, AZ - Owner
-      <br/>  12/2013 - 04/2021
-      <br/>  •  Owner
-      <br/>  •  Solo Game Developer
+           <b>Owner - Game Developer</b>
+      <br/>   Exploding Brain Games LLC - Phoenix, AZ  (12/2013 – 04/2021)
+      <br/>•  Owner
+      <br/>•  Game Developer
+      <br/>•  Software Engineer
       <br/>
-      <br/>  CaptiveMotion LLC, Tempe, AZ - Performance Capture QA
-      <br/>  08/2009 - 06/2011
-      <br/>  •  Performance Capture Data Clean-Up
-      <br/>  •  Performance Capture Camera Calibration
-      <br/>  •  Performance Capture Marker and Actor Preparation
+      <br/><b>Performance Capture QA Intern</b>
+      <br/>   CaptiveMotion LLC - Tempe, AZ  (08/2009 – 06/2011)
+      <br/>•  Performance Capture QA Technician
+      <br/>•  Performance Capture Camera Calibration Technician
     </p>
 
     <h2>EDUCATION</h2>
     <p>
-             ITT Technical Institute, Tempe, AZ - Bachelor’s Degree
-      <br/>  03/2009 - 03/2011
-      <br/>  Bachelor of Computer Science in Digital Entertainment and Game Design
+             Bachelor of Computer Science in Digital Entertainment and Game Design  (03/2009 – 03/2011)
       <br/>  GPA 3.42
       <br/>
-      <br/>  ITT Technical Institute, Tempe, AZ - Associate’s Degree
-      <br/>  03/2007 - 03/2009
-      <br/>  Associate of Computer-Aided Design and Drafting
+      <br/>  Associate of Applied Science in Computer Aided Drafting and Design  (03/2007 – 03/2009)
       <br/>  GPA 3.35
     </p>
   </Layout>
