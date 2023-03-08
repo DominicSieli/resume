84ad7e9f03bcd3c4429ae6f1d4380dd4ad90f6b5
DominicSieli <dominicsieli@gmail.com>
2023-03-08 12:40:56 -0700

    Reverted entire project

---
+++ src/pages/contacts.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const SecondPage = () => (
+  <Layout>
+    <h1>Contacts</h1>
+    <p>
+           Phone: (602) 448-9528
+      <br/>Email: dominicsieli@gmail.com
+    </p>
+  </Layout>
+)
+
+export default SecondPage
---
+++ src/pages/index.js
@@ -0,0 +1,64 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+	<SEO title="Resume" />
+	<h1>Resume</h1>
+
+	<h2>ACCREDITATION</h2>
+	<p>
+		   •  <a href="https://www.microsoft.com/en-us/p/vector-blade/9pj4rv8ngjbn?activetab=pivot:overviewtab">Vector Blade</a> (Xbox One) - Exploding Brain Games, 2019
+	  <br/>•  <a href="https://play.google.com/store/apps/details?id=com.Exploding_Brain_Games.Space_Shark_Free">Space Shark</a> (Google Play) - Exploding Brain Games, 2015
+	  <br/>•  <a href="https://play.google.com/store/apps/details?id=com.Exploding_Brain.Munchy_Mo_Demo">Munchy Mo</a> (Google Play) - Exploding Brain Games, 2014
+	  <br/>•  <a href="https://www.mobygames.com/developer/sheet/view/developerId,426098/">Alan Wake</a> (Xbox 360) - Remedy Entertainment, 2010
+	</p>
+
+	<h2>SKILLS</h2>
+	<p>
+		   <b>Programming</b>
+	  <br/>•  Experience with C, C++, and C#
+	  <br/>•  Strong Knowledge of Data-Structure and Algorithm Design
+	  <br/>
+	  <br/><b>Game Development</b>
+	  <br/>•  Experience with UI Design
+	  <br/>•  Experience with Animation
+	  <br/>•  Experience with 3D Modeling
+	  <br/>•  Experience with Game Play Design
+	  <br/>•  10 Years of Unity Game Engine Experience
+	  <br/>
+	  <br/><b>Publishing</b>
+	  <br/>•  Experience Publishing to Xbox One
+	  <br/>•  Experience Publishing to Google Play Store
+	</p>
+
+	<h2>WORK EXPERIENCE</h2>
+	<p>
+		   <b>Owner - Game Developer</b>
+	  <br/><b>Exploding Brain Games LLC - Phoenix, AZ  (12/2013 – 04/2021)</b>
+	  <br/>•  Owner
+	  <br/>•  Game Developer
+	  <br/>•  Software Engineer
+	  <br/>
+	  <br/><b>Performance Capture QA Intern</b>
+	  <br/><b>CaptiveMotion LLC - Tempe, AZ  (08/2009 – 06/2010)</b>
+	  <br/>•  Performance Capture QA Technician
+	  <br/>•  Performance Capture Camera Calibration Technician
+	</p>
+
+	<h2>EDUCATION</h2>
+	<p>
+			 Bachelor of Computer Science in Digital Entertainment and Game Design  (03/2009 – 03/2011)
+	  <br/>  GPA 3.42
+	  <br/>
+	  <br/>  Associate of Applied Science in Computer Aided Drafting and Design  (03/2007 – 03/2009)
+	  <br/>  GPA 3.35
+	</p>
+  </Layout>
+)
+
+export default IndexPage
